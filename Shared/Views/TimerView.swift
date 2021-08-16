7b65cf696a87841f1d33b40b0d1f23475de159a4
captain-camel <cameron.delong@icloud.com>
2021-08-16 11:23:52 -0600

    Added `TimerView` view

---
+++ Shared/Views/TimerView.swift
@@ -0,0 +1,15 @@
+//
+//  TimerView.swift
+//  Cubing Timer
+//
+//  Created by Cameron Delong on 8/16/21.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text("placeholder")
+            .font(.system(size: 100, design: .monospaced))
+    }
+}
