f065211e8335059ad6505e187de87f2eedd9d6f9
captain-camel <cameron.delong@icloud.com>
2021-08-16 14:29:27 -0600

    Moved timer logic to `TimerView`

--- Shared/Views/InstanceView.swift
+++ Shared/Views/InstanceView.swift
@@ -13,15 +13,10 @@ struct InstanceView: View {
     /// The `Instance` displayed by the `InstanceView`.
     @ObservedObject var instance: Instance
     
-    /// A stopwatch to time solves.
-    @ObservedObject var stopwatch = Stopwatch()
-    /// A countdown to start timing solves.
-    @ObservedObject var countdown = Countdown(duration: 0.5)
-    /// A timer for inspection.
-    @ObservedObject var inspection = Inspection(duration: 15)
-    
     /// The state of any drag gesture in progress.
     @State var gestureState: GestureState = .none
+    
+//    @State var timerState: TimerView.TimerState = .none
     
 //    /// All of the `Solve`s fetched from Core Data belonging to the current `Instance`.
 //    @FetchRequest var solves: FetchedResults<Solve>
@@ -40,8 +35,7 @@ struct InstanceView: View {
     // MARK: Body
     var body: some View {
         VStack {
-            Text(Solve.formatTime(stopwatch.secondsElapsed))
-            Text(countdown.isRunning.description)
+            TimerView(gestureState: $gestureState)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .contentShape(Rectangle())
@@ -51,72 +45,39 @@ struct InstanceView: View {
                     let horizontalDistance = gesture.translation.width
                     let verticalDistance = gesture.translation.height
                     
-                    switch gestureState {
-                    case .none:
-                        if stopwatch.isRunning {
-                            gestureState = .complete
-                            
-                            try? stopwatch.stop()
-                            
-                            // TODO: Add solve here
-                        } else {
-                            gestureState = .stationary
-                            
-                            try? countdown.start()
-                        }
-                        
-                    case .stationary:
-                        if !countdown.complete && (pow(gesture.translation.height, 2) + pow(gesture.translation.width, 2)).squareRoot() > 15 {
-                            if abs(horizontalDistance) > abs(verticalDistance) {
-                                if horizontalDistance < 0 {
-                                    gestureState = .moved(.left, horizontalDistance)
-                                } else {
-                                    gestureState = .moved(.right, horizontalDistance)
-                                }
+                    let totalDistance = (pow(gesture.translation.height, 2) + pow(gesture.translation.width, 2)).squareRoot()
+                    
+                    if totalDistance < 15 {
+                        gestureState = .stationary
+                    } else if (pow(gesture.translation.height, 2) + pow(gesture.translation.width, 2)).squareRoot() > 15 && (gestureState == .none || gestureState == .stationary) {
+                        print(gestureState)
+                        if abs(horizontalDistance) > abs(verticalDistance) {
+                            if horizontalDistance < 0 {
+                                gestureState = .moved(.left, horizontalDistance)
                             } else {
-                                if verticalDistance < 0 {
-                                    gestureState = .moved(.up, verticalDistance)
-                                } else {
-                                    gestureState = .moved(.down, verticalDistance)
-                                }
+                                gestureState = .moved(.right, horizontalDistance)
                             }
-                            
-                            try? countdown.reset()
+                        } else {
+                            if verticalDistance < 0 {
+                                gestureState = .moved(.up, verticalDistance)
+                            } else {
+                                gestureState = .moved(.down, verticalDistance)
+                            }
                         }
-                        
-                    case let .moved(direction, _):
-                        switch direction {
-                        case .left, .right:
-                            gestureState = .moved(direction, horizontalDistance)
-                        case .up, .down:
-                            gestureState = .moved(direction, verticalDistance)
-                        }
-                        
-                    default: break
                     }
                 }
                 .onEnded { gesture in
-                    switch gestureState {
-                    case .stationary:
-                        if countdown.complete {
-                            try? stopwatch.start()
-                        }
-                        
-                        try? countdown.reset()
-                        
-                    case let .moved(direction, distance):
-                        switch direction {
-                        case .left:
-                            print("left", distance)
-                        case .right:
-                            print("right", distance)
-                        case .up:
-                            print("up", distance)
-                        case .down:
-                            print("down", distance)
-                        }
-                        
-                    default: break
+                    switch gestureState.direction {
+                    case .right:
+                        print("right")
+                    case .left:
+                        print("left")
+                    case .up:
+                        print("up")
+                    case .down:
+                        print("down")
+                    default:
+                        break
                     }
                     
                     gestureState = .none
@@ -124,17 +85,59 @@ struct InstanceView: View {
         )
     }
     
-    enum GestureState {
+    // MARK: Types
+    /// Possible states for a `DragGesture` to be in.
+    enum GestureState: Equatable {
+        // MARK: Cases
+        /// No gesture in progress.
         case none
+        /// Gesture started but not moved.
         case stationary
+        /// Gesture moved.
         case moved(GestureDirection, CGFloat)
-        case complete
+
+        /// A unique if for each `GestureState`.
+        var id: Int {
+            switch self {
+            case .none:
+                return 0
+            case .stationary:
+                return 1
+            case .moved:
+                return 2
+            }
+        }
+        
+        /// The distance that has been swiped left, right, up, or down.
+        var distance: CGFloat? {
+            switch self {
+            case let .moved(_, distance):
+                return distance
+            default:
+                return nil
+            }
+        }
+        
+        /// The direction that was swiped.
+        var direction: GestureDirection? {
+            switch self {
+            case let .moved(direction, _):
+                return direction
+            default:
+                return nil
+            }
+        }
     }
     
+    /// Possible directions for a `DragGesture` to be in.
     enum GestureDirection {
+        /// Swiped left.
         case left
+        /// Swiped right.
         case right
+        /// Swiped up.
         case up
+        /// Swiped down.
         case down
     }
 }
--- Shared/Views/TimerView.swift
+++ Shared/Views/TimerView.swift
@@ -8,8 +8,63 @@
 import SwiftUI
 
 struct TimerView: View {
+    // MARK: Properties
+    /// A stopwatch to time solves.
+    @ObservedObject var stopwatch = Stopwatch()
+    /// A countdown to start timing solves.
+    @ObservedObject var countdown = Countdown(duration: 0.5)
+    /// A timer for inspection.
+    @ObservedObject var inspection = Inspection(duration: 15)
+    
+    /// A binding to a `GestureState` to determine when to start and stop the timer.
+    @Binding var gestureState: InstanceView.GestureState {
+        didSet {
+            switch gestureState {
+            case .none:
+                if countdown.isComplete {
+                    try? stopwatch.start()
+                }
+                
+                try? countdown.reset()
+            case .stationary:
+                if stopwatch.isRunning {
+                    try? stopwatch.stop()
+                } else {
+                    try? countdown.start()
+                }
+                
+            case .moved:
+                try? countdown.reset()
+            }
+        }
+    }
+    
+    // MARK: Body
     var body: some View {
-        Text("placeholder")
-            .font(.system(size: 100, design: .monospaced))
+        VStack {
+            Text(Solve.formatTime(stopwatch.secondsElapsed))
+            Text(String(countdown.isRunning))
+            Text(String(gestureState.id))
+        }
+        .font(.system(size: 100, design: .monospaced))
+        .onChange(of: gestureState.id) { _ in
+            switch gestureState {
+            case .none:
+                if countdown.isComplete {
+                    try? stopwatch.start()
+                }
+                
+                try? countdown.reset()
+            case .stationary:
+                if stopwatch.isRunning {
+                    try? stopwatch.stop()
+                } else {
+                    try? countdown.start()
+                }
+                
+            case .moved:
+                try? countdown.reset()
+            }
+        }
     }
 }
