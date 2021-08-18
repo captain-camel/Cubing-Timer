67ee609595a87552ff0969da779459a4ccba9fa3
captain-camel <cameron.delong@icloud.com>
2021-08-18 16:27:01 -0600

    Moved rows in `ListPopover` to `PopoverRow` view

--- Shared/Views/ListPopover.swift
+++ Shared/Views/ListPopover.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-/// A popover displaing a list of items with a limit of 10.
+/// A popover displaing a list of items.
 struct ListPopover: View {
     // MARK: Properties
     /// The array of vales to display.
@@ -19,50 +19,34 @@ struct ListPopover: View {
     /// A closure that is called when the button next to a row is pressed.
     var action: ((_ index: Int) -> Void)?
     /// The SF symbol on the button in each row.
-    var actionSymbolName: String?
+    var actionSymbol: String?
     
     // MARK: Initializers
-    init(values: [String], limit: Int = 10) {
+    init(values: [String], limit: Int = 10, action: ((_ index: Int) -> Void)? = nil, actionSymbol: String? = nil) {
         if values.count > limit {
             self.values = values.suffix(limit - 1) + ["\(values.count - (limit - 1)) more..."]
             valuesConcatenated = true
-            
         } else {
             self.values = values
         }
+        
+        self.action = action
+        self.actionSymbol = actionSymbol
     }
     
     // MARK: Body
     var body: some View {
-        VStack {
-            ForEachWithIndex(values, id: \.self) { index, value in
-                HStack {
-                    if !(index == values.indices.last && valuesConcatenated) {
-                        Text("\(index).")
-                            .foregroundColor(.secondary)
+        VStack(spacing: 0) {
+            ForEach(Array(values.enumerated()), id: \.offset) { index, value in
+                PopoverRow(index: !((index == values.indices.last!) && valuesConcatenated) ? index : nil, value: value, action: action, actionSymbol: actionSymbol)
+                    .if(index % 2 == 0) { view in
+                        view.background(
+                            Color(.systemGray5)
+                                .cornerRadius(5)
+                        )
                     }
-
-                    Text(value)
-
-                    if action != nil && !(index == values.indices.last && valuesConcatenated) {
-                        Spacer()
-
-                        Button {
-                            action!(index)
-                        } label: {
-                            Image(systemName: actionSymbolName ?? "questionmark")
-                        }
-                        .frame(alignment: .trailing)
-                        .padding()
-                    }
-                }
-                .if((values.count - index) % 2 == 0) { view in
-                    view.background(
-                        Color(.systemGray5)
-                            .cornerRadius(5)
-                    )
-                }
             }
         }
+        .padding()
     }
 }
