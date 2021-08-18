1e6543de958b4b7b2ed0cdec1d558e7be3a5ea0a
captain-camel <cameron.delong@icloud.com>
2021-08-18 16:27:53 -0600

    Moved rows in `ListPopover` to `PopoverRow` view

---
+++ Shared/Views/PopoverRow.swift
@@ -0,0 +1,46 @@
+//
+//  PopoverRow.swift
+//  Cubing Timer
+//
+//  Created by Cameron Delong on 8/18/21.
+//
+
+import SwiftUI
+
+struct PopoverRow: View {
+    /// The `Int` shown at the start of the row.
+    var index: Int?
+    /// The `String` shown in the row.
+    var value: String
+    
+    /// A closure that is called when the button in the `PopoverRow` is pressed.
+    var action: ((_ index: Int) -> Void)?
+    /// The SF symbol on the button in the `PopoverRow`.
+    var actionSymbol: String?
+    
+    // MARK: Body
+    var body: some View {
+        HStack {
+            if index != nil {
+                Text("\(index! + 1).")
+                    .foregroundColor(.secondary)
+                    .padding(.leading)
+            }
+
+            Text(value)
+                .padding()
+            
+            Spacer()
+
+            if action != nil && index != nil {
+                Button {
+                    action!(index!)
+                } label: {
+                    Image(systemName: actionSymbol ?? "questionmark")
+                }
+                .frame(alignment: .trailing)
+                .padding()
+            }
+        }
+    }
+}
