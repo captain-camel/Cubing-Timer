8061476a8dc1194e622d7c6dc7706ce4b031d7a7
captain-camel <cameron.delong@icloud.com>
2021-08-19 17:30:55 -0600

    Added horizontal padding to `StatisticButtonStyle`

--- Shared/Miscellaneous/Button Styles/StatisticButtonStyle.swift
+++ Shared/Miscellaneous/Button Styles/StatisticButtonStyle.swift
@@ -12,6 +12,7 @@ struct StatisticButtonStyle: ButtonStyle {
     // MARK: Body
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
+            .padding(.horizontal, 4)
             .background(configuration.isPressed ? Color(.systemGray5) : .clear)
             .cornerRadius(5)
     }
