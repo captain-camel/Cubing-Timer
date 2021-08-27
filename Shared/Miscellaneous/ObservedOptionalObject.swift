68499f6827aa22f0a4b2122b4c6512ded74198b1
captain-camel <cameron.delong@icloud.com>
2021-08-27 11:02:42 -0600

    Fixed formatting in `ObservedOptionalObject.swift`

--- Shared/Miscellaneous/ObservedOptionalObject.swift
+++ Shared/Miscellaneous/ObservedOptionalObject.swift
@@ -34,27 +34,27 @@ import SwiftUI
 /// ```
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 @propertyWrapper public struct ObservedOptionalObject<T: ObservableObject>: DynamicProperty {
-
     @StateObject private var proxy = Proxy()
-
+    
     /// The Proxy class holds the optional observed object and republisheds it's `objectWillChange`
     /// events.
     private class Proxy: ObservableObject {
-        var wrappedObject: T? { didSet {
-
-            // Update the publisher if the objects identity change or if
-            // either the old value or the new value are `nil`
-            if let wrappedObject = wrappedObject, let oldValue = oldValue {
-                if ObjectIdentifier(wrappedObject) != ObjectIdentifier(oldValue) {
+        var wrappedObject: T? {
+            didSet {
+                // Update the publisher if the objects identity change or if
+                // either the old value or the new value are `nil`
+                if let wrappedObject = wrappedObject, let oldValue = oldValue {
+                    if ObjectIdentifier(wrappedObject) != ObjectIdentifier(oldValue) {
+                        updatePublisher()
+                    }
+                } else {
                     updatePublisher()
                 }
-            } else {
-                updatePublisher()
             }
-        } }
-
+        }
+        
         private var cancellable: AnyCancellable?
-
+        
         private func updatePublisher() {
             cancellable?.cancel()
             cancellable = wrappedObject?
@@ -64,29 +64,28 @@ import SwiftUI
                 }
         }
     }
-
+    
     /// The observed object itself.
     private(set) public var wrappedValue: T?
-
+    
     /// Create a new ObservedOptionalObject with an initial value
     public init(initialValue: T?) {
         self.wrappedValue = initialValue
     }
-
+    
     /// Create a new ObservedOptionalObject. Don't call this initializer directly.
     /// Instead use `@OptionalObservedObject`
     public init(wrappedValue: T?) {
         self.wrappedValue = wrappedValue
     }
-
+    
     public func update() {
         proxy.wrappedObject = wrappedValue
     }
-
+    
     @dynamicMemberLookup public struct Wrapper {
-
         let wrappedObject: T?
-
+        
         /// Returns an optional binding to the resulting value of a given key path.
         ///
         /// - Parameter keyPath  : A key path to a specific resulting value.
@@ -94,7 +93,7 @@ import SwiftUI
         /// - Returns: A new binding .
         public subscript<Subject>(dynamicMember keyPath: ReferenceWritableKeyPath<T, Subject>) -> Binding<Subject>? {
             guard let wrappedObject = wrappedObject else { return nil }
-
+            
             return Binding {
                 wrappedObject[keyPath: keyPath]
             } set: { value in
@@ -102,10 +101,9 @@ import SwiftUI
             }
         }
     }
-
+    
     /// A projection to create bindings to the observed object
     public var projectedValue: Wrapper {
         Wrapper(wrappedObject: wrappedValue)
     }
-
 }
