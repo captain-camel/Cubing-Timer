fec3407b22ce502577614b71fe8775c34c5ca081
captain-camel <cameron.delong@icloud.com>
2021-09-08 11:35:55 -0600

    Added `Settings` view

---
+++ Shared/Views/Settings.swift
@@ -0,0 +1,20 @@
+//
+//  Settings.swift
+//  Cubing Timer
+//
+//  Created by Cameron Delong on 9/8/21.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text("placeholder")
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
