b842f7d0ac5a35ede5c432acdf429dc1a7093bb6
captain-camel <cameron.delong@icloud.com>
2021-09-08 18:54:38 -0600

    Added haptics and color settings to `SettingsView`

--- Shared/Views/SettingsView.swift
+++ Shared/Views/SettingsView.swift
@@ -7,9 +7,42 @@
 
 import SwiftUI
 
+/// A view that displays global settings.
 struct SettingsView: View {
+    // MARK: Properties
+    /// The color of the background when the timer is running.
+    @AppStorage("runningColor") var runningColor = Color(red: 0.27, green: 0.95, blue: 0.65)
+    /// The color of the background during inspection.
+    @AppStorage("inspectionColor") var inspectionColor = Color.yellow
+    /// Whether the app should have haptic feedback.
+    @AppStorage("doHaptics") var doHaptics = true
+    
+    // MARK: Body
     var body: some View {
-        Text("placeholder")
+        Form {
+            Section(header: Text("Colors"), footer: Text("These are the colors of the background when timing solves.")) {
+                ColorPicker("Timer Background", selection: $runningColor, supportsOpacity: false)
+                
+                ColorPicker("Inspection Background", selection: $inspectionColor, supportsOpacity: false)
+            }
+            
+            Section(header: Text("Haptic Feedback")) {
+                Toggle("Haptic Feedback", isOn: $doHaptics)
+            }
+        }
+        .navigationTitle("Settings")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Restore Default") {
+                    withAnimation {
+                        runningColor = Color(red: 0.27, green: 0.95, blue: 0.65)
+                        inspectionColor = Color.yellow
+                        
+                        doHaptics = true
+                    }
+                }
+            }
+        }
     }
 }
 
