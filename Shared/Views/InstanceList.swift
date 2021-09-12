2bc063d5cd7e3051fb2476f5c98ec4a4885861fb
captain-camel <cameron.delong@icloud.com>
2021-09-12 10:07:07 -0600

    Added `navigationController` property to `InstanceList` view

--- Shared/Views/InstanceList.swift
+++ Shared/Views/InstanceList.swift
@@ -16,6 +16,9 @@ struct InstanceList: View {
     /// Whether the sheet to add a new instance is displayed.
     @State private var showingAddInstanceSheet = false
     
+    /// The `UIKit` `UINavigationController` behind the `NavigationView`.
+    @State private var navigationController: UINavigationController?
+    
     // MARK: Body
     var body: some View {
         List {
@@ -59,6 +62,12 @@ struct InstanceList: View {
                     inspectionDuration: inspectionDuration
                 )
             }
+        }
+        .introspectNavigationController { navigationController in
+            self.navigationController = navigationController
+        }
+        .onAppear {
+            navigationController?.interactivePopGestureRecognizer?.isEnabled = true
         }
     }
 }
