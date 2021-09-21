d9447035917ade2d21fbc99838ea3ad53843c48c
captain-camel <cameron.delong@icloud.com>
2021-09-20 21:44:16 -0600

    Added `CodeView.swift`

---
+++ Shared/Views/Support/CodeView.swift
@@ -0,0 +1,20 @@
+//
+//  CodeView.swift
+//  CodeView
+//
+//  Created by Cameron Delong on 9/20/21.
+//
+
+import SwiftUI
+
+struct CodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CodeView_Previews: PreviewProvider {
+    static var previews: some View {
+        CodeView()
+    }
+}
