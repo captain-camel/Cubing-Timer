cc4844aed259ddb16eeedade55879e0487597a42
captain-camel <cameron.delong@icloud.com>
2021-09-21 21:18:09 -0600

    Added content to `CodeView`

--- Shared/Views/Support/CodeView.swift
+++ Shared/Views/Support/CodeView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct CodeView: View {
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(Color(.systemGray6))
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.secondary, lineWidth: 1)
+            
+            HStack {
+                Text("test\ntestttt")
+                    .multilineTextAlignment(.leading)
+                    .font(.system(.body, design: .monospaced))
+                    .padding()
+                
+                Spacer()
+            }
+        }
+        .fixedSize(horizontal: false, vertical: true)
     }
 }
 
 struct CodeView_Previews: PreviewProvider {
     static var previews: some View {
         CodeView()
+            .padding()
+            
+        
     }
 }
