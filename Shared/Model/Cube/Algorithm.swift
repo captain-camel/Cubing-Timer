083b277c3013244fc364824ff2d65c0be089a38c
captain-camel <cameron.delong@icloud.com>
2021-09-26 12:16:16 -0600

    Updated `Algorithm` to conform to `Codable` and `RawRepresentable`

--- Shared/Model/Cube/Algorithm.swift
+++ Shared/Model/Cube/Algorithm.swift
@@ -5,13 +5,19 @@
 //  Created by Cameron Delong on 8/13/21.
 //
 
+import OrderedCollections
+import Foundation
+
 /// A series of `Moves` forming an `Algorithm` that can be performed on a `Cube`.
-struct Algorithm {
+struct Algorithm: Codable {
     // MARK: Properties
     static var algorithms: [Algorithm] = [""]
     
     /// The `Move`s making up the `Algorithm`.
     var moves: [Move] = []
+    
+    /// The locations of "triggers" in the `Algorithm`.
+    var triggerIndices: [Int] = []
     
     /// The reverse of the algorithm.
     var reversed: Algorithm {
@@ -68,7 +74,33 @@ extension Algorithm: LosslessStringConvertible {
     // MARK: Properties
     /// A `String` describing of the `Algorithm`.
     var description: String {
-        return moves.map { String($0) }.joined(separator: " ")
+        var elements: [String] = []
+        
+        var triggerCharacter = "("
+        
+        for (index, move) in moves.enumerated() {
+            if triggerIndices.contains(index) {
+                elements.append(triggerCharacter)
+                
+                triggerCharacter = triggerCharacter == "(" ? ")" : "("
+            }
+            
+            elements.append(String(move))
+        }
+        
+        var description = ""
+        
+        for (index, element) in elements.enumerated() {
+            description.append(element)
+            
+            if index != elements.endIndex - 1 && element != "(" && elements[safe: index + 1] != ")" {
+                description.append(" ")
+            }
+            
+            
+        }
+        
+        return description
     }
     
     // MARK: Initializers
@@ -76,12 +108,22 @@ extension Algorithm: LosslessStringConvertible {
     init(_ description: String) {
         let moves = description.split(separator: " ")
         
+        var index = 0
+        
         for move in moves {
-            guard let toAppend = Move(String(move)) else {
-                break
+            if move.first == "(" || move.first == ")" {
+                triggerIndices.append(index)
+            } else if move.last == "(" || move.last == ")" {
+                triggerIndices.append(index + 1)
+            }
+            
+            guard let toAppend = Move(String(move.filter { $0 != "(" && $0 != ")" })) else {
+                continue
             }
             
             self.moves.append(toAppend)
+            
+            index += 1
         }
     }
 }
@@ -97,5 +139,36 @@ extension Algorithm: ExpressibleByArrayLiteral {
     // MARK: Initializers
     init(arrayLiteral: Move...) {
         self.init(arrayLiteral)
+    }
+}
+
+extension Algorithm: Hashable {
+    // MARK: Methods
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(moves)
+    }
+}
+
+extension Algorithm: RawRepresentable {
+    // MARK: Initializers
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode(Self.self, from: data)
+        else {
+            return nil
+        }
+        
+        self = result
+    }
+    
+    // MARK: Properties
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return ""
+        }
+        
+        return result
     }
 }
