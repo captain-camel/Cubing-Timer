17c0896f9f9a14f5371b6def2cea2b9f118e4aa5
captain-camel <cameron.delong@icloud.com>
2021-09-29 20:13:29 -0600

    Added `StatisticCard` view

---
+++ Shared/Views/StatisticCard.swift
@@ -0,0 +1,56 @@
+//
+//  StatisticCard.swift
+//  StatisticCard
+//
+//  Created by Cameron Delong on 9/29/21.
+//
+
+import SwiftUI
+
+/// A card displaying a `Statistic`.
+struct StatisticCard: View {
+    // MARK: Properties
+    /// The `Statistic` described by the `StatisticCard`.
+    var statistic: Statistic
+    /// The `Instance` who's `Statistic` is displayed.
+    @ObservedObject var instance: Instance
+    
+    // MARK: Initializers
+    init(_ statistic: Statistic, instance: Instance) {
+        self.statistic = statistic
+        self.instance = instance
+    }
+    
+    // MARK: Body
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(Color.white)
+                .shadow(radius: 10)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(statistic.longName)
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    
+                    if let systemName = statistic.symbol(of: instance) {
+                        Image(systemName: systemName)
+                            .foregroundColor(statistic.symbolColor(of: instance))
+                    }
+                }
+                
+                Text(statistic.value(of: instance))
+                    .font(.title)
+                    .fontWeight(.black)
+            }
+        }
+    }
+}
+
+struct StatisticCard_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticCard(Statistic.personalBest, instance: Instance())
+    }
+}
+
