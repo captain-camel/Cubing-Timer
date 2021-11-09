cb2f0703da99af28cfc3d371d9baf1688cc50a4d
captain-camel <cameron.delong@icloud.com>
2021-11-08 18:41:03 -0700

    Added `HUDManager` class

---
+++ Shared/Miscellaneous/HUDManager.swift
@@ -0,0 +1,39 @@
+//
+//  HUDManager.swift
+//  Cubing Timer
+//
+//  Created by Cameron Delong on 11/8/21.
+//
+
+import Combine
+import Foundation
+
+/// A class that manages app-wide notifications in a `HUD`.
+class HUDManager: ObservableObject {
+    /// The HUDs that are currently displayed.
+    @Published var huds: [HUD] = []
+    
+    /// Displays a HUD over the UI with text and a system image.
+    func showHUD(text: String, systemImage: String, duration: Double = 2) {
+        huds.append(HUD(text: text, systemImage: systemImage))
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+            self.huds.remove(at: self.huds.firstIndex(of: HUD(text: text, systemImage: systemImage)) ?? 0)
+        }
+    }
+    
+    /// Displays a HUD over the UI with text.
+    func showHUD(text: String, duration: Double = 2) {
+        huds.append(HUD(text: text, systemImage: nil))
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+            self.huds.remove(at: self.huds.firstIndex(of: HUD(text: text, systemImage: nil)) ?? 0)
+        }
+    }
+    
+    /// A `struct` to represent a currently displayed HUD.
+    struct HUD: Equatable, Hashable {
+        var text: String
+        var systemImage: String?
+    }
+}
