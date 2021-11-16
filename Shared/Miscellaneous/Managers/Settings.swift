716ac0a50c9ed2e86612fb053ad4afb675c9901a
captain-camel <cameron.delong@icloud.com>
2021-11-15 19:34:27 -0700

    Removed `Setting` property wrapper

--- Shared/Miscellaneous/Managers/Settings.swift
+++ Shared/Miscellaneous/Managers/Settings.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 /// A class to access all of the global settings of the app.
-final class Settings: AppSettings, KeyPathListable {
+final class Settings: AppSettings {
     // MARK: Properties
     /// A singleton instance of `AppSettings`.
     static var shared = Settings()
@@ -19,35 +19,4 @@ final class Settings: AppSettings, KeyPathListable {
     @Field var runningColor = Color(red: 0.27, green: 0.95, blue: 0.65)
     /// The color of the background during inspection.
     @Field var inspectionColor: Color = .yellow
-    
-    // MARK: Initializers
-    private init() {}
-}
-
-@propertyWrapper struct Setting<T: Codable> {
-    let keyPath: WritableKeyPath<Settings, T>
-    
-    var wrappedValue: T {
-        get {
-            return Settings.shared[keyPath: keyPath]
-        }
-        set {
-            Settings.shared[keyPath: keyPath] = newValue
-        }
-    }
-    
-    var projectedValue: Binding<T> {
-        Binding(
-            get: {
-                return Settings.shared[keyPath: keyPath]
-            },
-            set: { newValue in
-                Settings.shared[keyPath: keyPath] = newValue
-            }
-        )
-    }
-    
-    init(_ keyPath: WritableKeyPath<Settings, T>) {
-        self.keyPath = keyPath
-    }
 }
