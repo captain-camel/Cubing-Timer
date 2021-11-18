399a47961428f47a39d2d9707fc07b34c3318e53
captain-camel <cameron.delong@icloud.com>
2021-11-17 21:52:04 -0700

    Fixed typo in `PropertyNameable` documentation

--- Shared/Miscellaneous/PropertyNameable.swift
+++ Shared/Miscellaneous/PropertyNameable.swift
@@ -12,7 +12,7 @@ extension PropertyNameable {
     // MARK: Methods
     /// Returns the name of a property that is a part of this class as a `String`.
     func name(of property: AnyObject) -> String {
-        let propertyAddress = Unmanaged.passUnretained(property).toOpaque() // Gets the momory address of the property
+        let propertyAddress = Unmanaged.passUnretained(property).toOpaque() // Gets the memory address of the property
         let children = Mirror(reflecting: self).children
         let matchingChildName = children.filter { child in
             propertyAddress == Unmanaged.passUnretained(child.value as AnyObject).toOpaque() // Gets the child that has the same memory address as the property
