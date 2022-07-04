a545ea5cbe204613fd70960cc35b21533ab3929d
Cameron Delong <cameron.delong@icloud.com>
2022-07-04 15:48:59 -0400

    Remove markdown renderer from `HelpView`

--- Shared/Views/HelpView.swift
+++ Shared/Views/HelpView.swift
@@ -6,37 +6,13 @@
 //
 
 import SwiftUI
-import MarkdownUI
 
 /// A view displaying help on how to use the app.
 struct HelpView: View {
-    // MARK: Properties
-    /// Markdown to display when help file fails to load.
-    let errorMessage = """
-                ## Uh oh!
-                There was an error loading help page...
-                """
-    
-    /// The markdown text to display
-    let text: String
-    
-    // MARK: Initializers
-    init() {
-        if let path = Bundle.main.path(forResource: "HELP", ofType: "md") {
-            do {
-                text = try String(contentsOfFile: path)
-            } catch {
-                text = errorMessage
-            }
-        } else {
-            text = errorMessage
-        }
-    }
-    
     // MARK: Body
     var body: some View {
         ScrollView {
-            Markdown(Document(text))
+            Text("TODO")
                 .padding()
         }
         .navigationTitle("Help")
